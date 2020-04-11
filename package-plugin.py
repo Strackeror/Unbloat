e82472acf79f22e4f25eadd6e5814c77833d0374
Strackeror <rremilavergne@gmail.com>
2020-04-11 11:13:34 +0200

    Initial commit

---
+++ package-plugin.py
@@ -0,0 +1,14 @@
+from zipfile import ZipFile
+import os
+import sys
+
+
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+if len(sys.argv) > 1:
+    pname = sys.argv[1]
+else:
+    pname = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+
+with ZipFile(f"x64/{pname}.zip", 'w') as zip:
+    zip.write(f"x64/Release/{pname}.dll", f"nativePC/plugins/{pname}.dll")
